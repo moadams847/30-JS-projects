ad3fcf2a78da16533d12116440b0b184759b7727
Adams Mohammed <moadams847@gmail.com>
2021-08-15 13:24:09 +0000

    create hex.js

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
 
-const btn = document.getElementById("btn");
-const color = document.getElementsByClassName("color");
+const btn = document.querySelector("#btn");
+const color = document.querySelector(".color");
 
 btn.addEventListener("click", function () {
-  console.log(getRandomNumber());
+  let randomNumber = getRandomNumber();
+  console.log(randomNumber);
 
-  document.body.style.backgroundColor = colors[getRandomNumber()];
-  color.textContent = colors[getRandomNumber()];
+  document.body.style.backgroundColor = colors[randomNumber];
+  color.textContent = colors[randomNumber];
 });
-
 function getRandomNumber() {
   return Math.floor(Math.random() * colors.length);
 }
--- hex.js
+++ hex.js
@@ -1,1 +1,24 @@
 const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+
+// hex colors
+// it has seven special characters
+// which starts with a #
+
+const btn = document.querySelector("#btn");
+const color = document.querySelector(".color");
+
+// each click goes through six iterations
+// it also calls the getRandomNumber fxn six times per click
+btn.addEventListener("click", function () {
+  let hexColor = "#";
+  for (let i = 0; i < 6; i++) {
+    let randomNumber = getRandomNumber();
+    hexColor += hex[randomNumber];
+  }
+  document.body.style.backgroundColor = hexColor;
+  color.textContent = hexColor;
+});
+
+function getRandomNumber() {
+  return Math.floor(Math.random() * hex.length);
+}
